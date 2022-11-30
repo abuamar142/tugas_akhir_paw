cb62504b18d9af1a466a328dc8ca52c445e3c39f
abuamar142 <amarjabon130@gmail.com>
2022-12-01 06:38:17 +0700

    tambah nama project

--- app.py
+++ app.py
@@ -7,5 +7,25 @@ application = Flask(__name__)
 def index():
     pass
 
+@application.route('/register')
+def register():
+    pass
+
+@application.route('/save_data_register')
+def save_data_register():
+    pass
+
+@application.route('/login')
+def login():
+    pass
+
+@application.route('/login_verification')
+def login_verification():
+    pass
+
+@application.route('/add_transaction')
+def add_transaction():
+    pass
+
 if __name__ == '__main__':
     application.run(debug=True)
